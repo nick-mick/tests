41edffc7b0ec6a52089ed283c1491e7b4d9da6de
nick-mick <imsong@ukr.net>
2015-09-20 11:57:21 +0200

    Implement a function in Python that receives a string as a parameter and returns the same string with a capital letter at the beginning of each word.

---
+++ capital_letter.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# Implement a function in Python that receives a string as a parameter and returns the same string with a capital letter at the beginning of each word.
+
+
+def capital_letter(str):
+    return str.title()
+
+print(capital_letter('lorem ipsum dolor sit amet.'))
